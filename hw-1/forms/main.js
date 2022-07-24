5738434dcff0d46bd16a8334d6948fed4685ecac
Shuxrat Mamedov <lider_20_10@mail.ru>
2022-07-24 18:32:25 +0300

    form 1.1

---
+++ hw-1/forms/main.js
@@ -0,0 +1,6 @@
+const forms = document.querySelectorAll('form')
+const inputs = document.querySelectorAll('input')
+const reset = document.querySelectorAll('.reset')
+const diligense = document.querySelectorAll('.diligence')
+const show = document.querySelectorAll('.show')
+
