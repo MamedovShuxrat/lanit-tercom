91142f9dcb08a590470d5f72339faeeac0265a70
Shuxrat Mamedov <lider_20_10@mail.ru>
2022-07-25 22:05:37 +0300

    url-1.3

--- hw-1/forms/main.js
+++ hw-1/forms/main.js
@@ -2,6 +2,15 @@ const forms = document.querySelectorAll('form')
 const inputs = document.querySelectorAll('input')
 const reset = document.querySelectorAll('.reset')
 const diligense = document.querySelectorAll('.diligence')
-const show = document.querySelectorAll('.show')
+const show = document.getElementsByClassName('.show')
+console.log(show);
+show.onclick = function () {
+    console.log('dsdsd');
+}
 
-// 
+for (let i = 0; i < show.length; i++) {
+    // show[i].onclick = function () {
+    //     console.log('dsdsd');
+    // }
+    
+}
