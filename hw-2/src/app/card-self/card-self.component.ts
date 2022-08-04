c651e60c120cc37254e67bd849228e15a92641ae
Shuxrat Mamedov <lider_20_10@mail.ru>
2022-08-04 23:18:59 +0300

    add moduls

---
+++ hw-2/src/app/card-self/card-self.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-card-self',
+  templateUrl: './card-self.component.html',
+  styleUrls: ['./card-self.component.scss']
+})
+export class CardSelfComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
